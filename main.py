cc13a586d4cf97c36380164583404b58b4316ad9
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-12-22 15:07:08 +0300

    Creted ratings variable

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main():
     soup = BeautifulSoup(html, 'html.parser')
     movietags = soup.select ("td.titleColumn")
     innermovietags = soup.select('td.titleColumn a')
+    rating_tags = soup.select ('td.posterColumn span [name=ir]')
    
     
     def get_year (movie_tag):
@@ -22,15 +23,23 @@ def main():
     years = [get_year(tag) for tag in movietags]
     actors_list = [tag['title'] for tag in innermovietags]
     titles = [tag.text for tag in innermovietags]
+    ratings = [float(tag ['data-value']) for tag in rating_tags]
 
 
-    inner_movietag0 = innermovietags[0]
-    print (inner_movietag0)
-    actors = inner_movietag0['title']
-    title = inner_movietag0.text
-    print (actors)
-    print(title)
-    
+
+    n_movies = len(titles)
+
+    while trye
+
+
+    # inner_movietag0 = innermovietags[0]
+    # print (inner_movietag0)
+    # actors = inner_movietag0['title']
+    # title = inner_movietag0.text
+    # print (actors)
+    # print(title)
+    ratin0 = rating_tags[0]
+    print(ratin0 ['data-value'])
     
     
     
