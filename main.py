cf3cc41e868d59421ed328a19c6b13237e8d4211
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-12-22 14:59:47 +0300

    Created movie variables

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ url = 'https://www.imdb.com/chart/top'
 def main():
     response = requests.get(url)
     html = response.text
-
+ 
     soup = BeautifulSoup(html, 'html.parser')
     movietags = soup.select ("td.titleColumn")
-    inner_movietags = soup.select('td.titleColumn a')
+    innermovietags = soup.select('td.titleColumn a')
    
     
     def get_year (movie_tag):
@@ -20,10 +20,16 @@ def main():
         year = moviesplit[-1]
         return year
     years = [get_year(tag) for tag in movietags]
+    actors_list = [tag['title'] for tag in innermovietags]
+    titles = [tag.text for tag in innermovietags]
 
-    inner_movietag0 = inner_movietags[0]
-    print (movietag0)
-    # title = tag['title']
+
+    inner_movietag0 = innermovietags[0]
+    print (inner_movietag0)
+    actors = inner_movietag0['title']
+    title = inner_movietag0.text
+    print (actors)
+    print(title)
     
     
     
